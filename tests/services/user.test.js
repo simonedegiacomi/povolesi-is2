34e276f6fd531c2a4fd8ad8bcf69adca13398b62
GiorgioSgl <giorgiosgl1997@gmail.com>
2018-11-27 22:21:47 +0100

    test for /users

--- tests/services/user.test.js
+++ tests/services/user.test.js
@@ -3,6 +3,7 @@ const Models      = require('../../src/models/index');
 
 beforeEach((done) => {
     Models.sequelize.sync().then(_ => done())
+
 });
 
 test('Should return zero users if no users are registered',  (done) => {
@@ -10,10 +11,11 @@ test('Should return zero users if no users are registered',  (done) => {
         expect(users).toEqual([]);
         done();
     });
-
 });
 
 describe('Test the user registration', () => {
+
+    let createdUser;
 
     test('It should register the new user', (done) => {
         UserService.registerUser({
@@ -22,10 +24,27 @@ describe('Test the user registration', () => {
             badgeNumber: "000000",
             password   : 'password'
         }).then(user => {
+            //TODO: da togliere appena troviamo un modo per cancellare il database ad ogni giro di test
+            createdUser = user;
+
             expect(user.id).toBeDefined();
             done();
         }).catch(console.log);
     });
+
+
+    test('Should return the registered users',  (done) => {
+        UserService.getAllUsers().then(users => {
+            let firstUser = users[0];
+        
+            expect(firstUser.name)        .toEqual('Mario Rossi');
+            expect(firstUser.email)       .toEqual('mario@rossi.it');
+            expect(firstUser.badgeNumber) .toEqual("000000");
+
+            done();
+        });
+    });
+
 
 });
 
