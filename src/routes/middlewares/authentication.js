4456e9e928eda59195390a93d680e4a9a8d97969
simonedegiacomi <simonedegiacomi97@gmail.com>
2018-11-29 09:51:24 +0100

    Use async/await in authentication

--- src/routes/middlewares/authentication.js
+++ src/routes/middlewares/authentication.js
@@ -2,29 +2,28 @@
 
 const User = require('../../models/index').User;
 
-module.exports = function (req, res, next) {
+module.exports = async function (req, res, next) {
     const token = req.header('X-API-TOKEN');
-
     if (token == null) {
         return res.status(401).send({
             'errorMessage': "Header 'X-API-TOKEN' is missing, therefore you're not authenticated"
         })
     }
 
-    User.find({
+    const user = await User.findOne({
         where: {
             authToken: token
         }
-    }).then(user => {
-        if (user == null) {
-            return res.status(401).send({
-                'errorMessage': "Invalid authentication token"
-            });
-        }
+    });
 
-        req.user = user;
 
-        next();
-    })
+    if (user == null) {
+        return res.status(401).send({
+            'errorMessage': "Invalid authentication token"
+        });
+    }
 
+    req.user = user;
+
+    next();
 };
