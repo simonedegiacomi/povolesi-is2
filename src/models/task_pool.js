46eba66932f0e9589829a0b4cb61b2678438a67e
GiorgioSgl <giorgiosgl1997@gmail.com>
2018-12-01 16:40:39 +0100

    add relation task has one taskPool

--- src/models/task_pool.js
+++ src/models/task_pool.js
@@ -1,5 +1,8 @@
-module.exports = function (sequelize, DataTypes) {
-    return sequelize.define('TaskPool', {
+const User = require('./user');
+const Task = require('./task');
+
+let TaskPool = function (sequelize, DataTypes) {
+    let TaskPool = sequelize.define('TaskPool', {
         id       : {
             type         : DataTypes.INTEGER(11),
             allowNull    : false,
@@ -9,16 +12,18 @@ module.exports = function (sequelize, DataTypes) {
         name     : {
             type     : DataTypes.STRING(200),
             allowNull: false
-        },
-        createdBy: {
-            type      : DataTypes.INTEGER(11),
-            allowNull : false,
-            references: {
-                model: 'user',
-                key  : 'id'
-            }
         }
-    }, {
-        tableName: 'task_pool'
     });
+
+    //il task pool viene creato da un solo utente
+    let User_ = User(sequelize, DataTypes);
+    TaskPool.belongsTo(User_, {as: 'createdBy'});
+
+    //questo aggiunge un fk in Task che segnala a quale task pool appartiene
+    let Task_ = Task(sequelize, DataTypes);
+    TaskPool.hasMany(Task_, {as: 'task-pool'})
+
+    return TaskPool;
 };
+
+module.exports = TaskPool;
