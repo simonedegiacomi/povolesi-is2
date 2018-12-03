be37e8a5d416c7b37b31104e25e52032ad3088dc
simonedegiacomi <simonedegiacomi97@gmail.com>
2018-12-03 08:44:09 +0100

    Add associations between task and task choices and also between answer and answer choice

--- src/models/answer_choice.js
+++ src/models/answer_choice.js
@@ -1,25 +1,28 @@
-
 module.exports = function (sequelize, DataTypes) {
-    return sequelize.define('AnswerChoice', {
-        id      : {
+    const AnswerChoice = sequelize.define('AnswerChoice', {
+        id    : {
             type         : DataTypes.INTEGER(11),
             allowNull    : false,
             primaryKey   : true,
             autoIncrement: true
         },
-        answerId: {
-            type      : DataTypes.INTEGER(11),
-            allowNull : false,
-            references: {
-                model: 'answer',
-                key  : 'id'
-            }
-        },
-        choice  : {
+        choice: {
             type     : DataTypes.INTEGER(11),
             allowNull: false
         }
     }, {
         tableName: 'answer_choice'
     });
+
+    AnswerChoice.associate = (models) => {
+        AnswerChoice.belongsTo(models.Answer, {
+            as        : 'answer',
+            foreignKey: {
+                name     : 'answerId',
+                allowNull: false
+            }
+        });
+    };
+
+    return AnswerChoice;
 };
--- src/models/task_choice.js
+++ src/models/task_choice.js
@@ -1,18 +1,10 @@
 module.exports = function (sequelize, DataTypes) {
-    return sequelize.define('TaskChoices', {
+    const TaskChoice = sequelize.define('TaskChoice', {
         id    : {
             type         : DataTypes.INTEGER(11),
             allowNull    : false,
             primaryKey   : true,
             autoIncrement: true
-        },
-        taskId: {
-            type      : DataTypes.INTEGER(11),
-            allowNull : false,
-            references: {
-                model: 'task',
-                key  : 'id'
-            }
         },
         choice: {
             type     : DataTypes.STRING(500),
@@ -21,4 +13,16 @@ module.exports = function (sequelize, DataTypes) {
     }, {
         tableName: 'task_choices'
     });
+
+    TaskChoice.associate = (models) => {
+        TaskChoice.belongsTo(models.Task, {
+            as        : 'task',
+            foreignKey: {
+                name     : 'taskId',
+                allowNull: false
+            }
+        });
+    };
+
+    return TaskChoice;
 };
