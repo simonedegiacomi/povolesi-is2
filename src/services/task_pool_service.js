74d96e653b4088336d5fa1100acb72b0d1696b57
GiorgioSgl <giorgiosgl1997@gmail.com>
2018-12-03 11:59:55 +0100

    add other test

--- src/services/task_pool_service.js
+++ src/services/task_pool_service.js
@@ -1,14 +1,28 @@
-const {TaskPool} = require('../models/index');
+const {TaskPool,User} = require('../models/index');
 
+var existUser = function(u){
+    let jsonArray = User.findAll({
+        where: {
+            id: u.id
+        }
+    })
+
+    return jsonArray.length
+}
 
 module.exports = {
 
     errors: {
-        NO_CREATOR_SPECIFIED: "no creator specified"
+        NO_CREATOR_SPECIFIED: "no creator specified",
+        NO_NAME: "task pool have no name",
+        USER_NOT_EXIST: "user not exist"
     },
 
     async createTaskPool(taskPool) {
-        if (taskPool.createdBy == null) {
+        if (taskPool.name == null) {
+            throw new Error(this.errors.NO_NAME);
+        }
+        else if (taskPool.createdBy == null) {
             throw new Error(this.errors.NO_CREATOR_SPECIFIED);
         }
 
@@ -29,14 +43,19 @@ module.exports = {
 
     async getMyTaskPool(userMe) {
 
+        if(existUser(userMe))
+            throw new Error(this.errors.USER_NOT_EXIST);
+
         //query SELECT * WHERE user=userMe
         const jsonArray = await TaskPool.findAll({
-            where: {
+           where: {
                 createdById: userMe.id
-            }
+           }
         })
 
         return jsonArray
     }
+
+    async getTaskPool(userMe)
 
 };
